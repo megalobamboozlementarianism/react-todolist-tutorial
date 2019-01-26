4ff2f07ed8f7892e18e9fd8d39302a32a16cf82c
Egg Sandwich <eggsandwich@Eggs-MacBook-Pro.local>
2019-01-25 18:13:17 -0700

    change header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header style={headerStyle}>
-      <h1>TodoList</h1>
-      <Link style={linkStyle} to="/">Home</Link> |
+      <h1>Abe's TodoList</h1>
+      <Link style={linkStyle} to="/">Home</Link> | 
       <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
