21ee4c4cdc8de89890be01a3f156d24ec8e60b71
Egg Sandwich <eggsandwich@Eggs-MacBook-Pro.local>
2019-01-25 17:56:38 -0700

    shorten up the placeholders

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=1')
       .then(res => this.setState({ todos: res.data }))
   }
   // Toggle Complete
